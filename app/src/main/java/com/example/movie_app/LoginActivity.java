ab6bdc9891e77422f2520fa7f73f28102ed7b202
algirdaskuslys <algirdas.kuslys@kitm.lt>
2021-10-07 14:53:53 +0300

    pridetas mygtuko paspaudimas, informacijos isvedimas vartotojui, perejimas tarp langu, validacija

--- app/src/main/java/com/example/movie_app/LoginActivity.java
+++ app/src/main/java/com/example/movie_app/LoginActivity.java
@@ -2,15 +2,64 @@ package com.example.movie_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText username, password;
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        // pridetas komentaras
+        final EditText username = (EditText) findViewById(R.id.username);
+        final EditText password = (EditText) findViewById(R.id.password);
+
+        Button login = (Button) findViewById(R.id.login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View focusView) {
+
+                boolean cancel = false;
+                focusView = null;
+
+                if (!Validation.isUsernameValid(username.getText().toString())) {
+                    username.setError(getString(R.string.login_invalid_username));
+                    cancel = true;
+                }
+
+                if (!Validation.isPasswordValid(password.getText().toString())) {
+                    password.setError(getString(R.string.login_invalid_password));
+                    cancel = true;
+                }
+
+                if (cancel) { // turime klaidų
+//                    focusView.requestFocus();
+                } else { // praėjome autentifikacija
+
+                    Toast.makeText(LoginActivity.this, username.getText().toString() + "\n" + password.getText().toString(),
+
+                            Toast.LENGTH_LONG).show();
+
+                    //perform action on click
+                    Intent goToSearchActivity = new Intent(LoginActivity.this, SearchActivity.class);
+                    startActivity(goToSearchActivity);
+
+                }//else
+
+
+            }//onClick
+        });
+
+
     }
-}   
+}
---
+++ app/src/main/java/com/example/movie_app/Validation.java
@@ -0,0 +1,27 @@
+package com.example.movie_app;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Validation {
+    public static boolean isUsernameValid(String credentials) {
+        final String CREDENTIALS_PATTERN = "^[a-zA-Z]{3,20}$";
+
+        Pattern pattern = Pattern.compile(CREDENTIALS_PATTERN);
+
+        Matcher matcher = pattern.matcher(credentials);
+
+        return matcher.matches();
+    }
+
+
+    public static boolean isPasswordValid(String credentials) {
+        final String CREDENTIALS_PATTERN = "^[a-zA-Z0-9.!@_]{5,20}$";
+
+        Pattern pattern = Pattern.compile(CREDENTIALS_PATTERN);
+
+        Matcher matcher = pattern.matcher(credentials);
+
+        return matcher.matches();
+    }
+}
